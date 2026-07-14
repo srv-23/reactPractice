a1283f7713122bae7ccf68e65bd0cd973fcccd8f
srv-23 <srvchourasia10@gmail.com>
2026-07-14 17:58:29 +0530

    07reactRouter

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+
+function Layout() {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ 07reactRouter/src/components/github/Github.jsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "react-router-dom"
+
+function Github() {
+    const data = useLoaderData()
+    // const [data,setData] = useState({})
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/srv-23')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    // }, [])
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Github Avatar" className="mx-auto rounded-full" />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubLoader = async () => {
+    const response = await fetch('https://api.github.com/users/srv-23')
+    return response.json()
+}
