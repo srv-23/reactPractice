ff9e55bf80e8655a643da74a47751fba46463d6a
srv-23 <srvchourasia10@gmail.com>
2026-07-15 10:28:57 +0530

    reduxToolkittodo

---
+++ reduxToolkittodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+
+  return (
+    <>
+      <h1>Todo App</h1>   
+      <AddTodo />
+      <Todos />   
+    </>
+  )
+}
+
+export default App
