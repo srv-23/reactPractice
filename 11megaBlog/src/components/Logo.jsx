2ff78b9c19296bc4d7ed3dc351a591508c089bb1
srv-23 <srvchourasia10@gmail.com>
2026-07-19 14:44:31 +0530

    11megaBlog

---
+++ 11megaBlog/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+function Logo({width = '100px'}) {
+  return (
+    <div style={{width}}>Logo</div>
+  )
+}
+
+export default Logo
---
+++ 11megaBlog/src/components/container/Container.jsx
@@ -0,0 +1,9 @@
+function Container({children}) {
+  return (
+    <div className="w-full max-w-7xl mx-auto p-4">
+      {children}
+    </div>
+  )
+}
+
+export default Container
